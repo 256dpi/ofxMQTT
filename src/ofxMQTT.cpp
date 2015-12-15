61014c3aeb55047f419f07ea11af49ed5a229f28
Joël Gähwiler <joel.gaehwiler@gmail.com>
2015-12-15 12:23:05 +0100

    first commit

---
+++ src/ofxMQTT.cpp
@@ -0,0 +1,262 @@
+#include "ofxMQTT.h"
+
+using namespace mosqpp;
+
+ofxMQTT::ofxMQTT() : mosquittopp()
+{
+    lib_init();
+    bConnected = false;
+    bAutoReconnect = true;
+}
+
+ofxMQTT::ofxMQTT(const ofxMQTT& mom)
+{
+    clientID = mom.clientID;
+    host = mom.host;
+    port = mom.port;
+    username = mom.username;
+    password = mom.password;
+    keepAlive = mom.keepAlive;
+}
+
+ofxMQTT & ofxMQTT::operator=(const ofxMQTT &mom)
+{
+    clientID = mom.clientID;
+    host = mom.host;
+    port = mom.port;
+    username = mom.username;
+    password = mom.password;
+    keepAlive = mom.keepAlive;
+
+    return *this;
+}
+
+ofxMQTT::ofxMQTT(string clientID, string host, int port, bool cleanSession) : mosquittopp(clientID.c_str(), cleanSession)
+{
+    lib_init();
+    this->clientID = clientID;
+    this->host = host;
+    this->port = port;
+    this->keepAlive = 60;
+}
+
+ofxMQTT::~ofxMQTT()
+{
+    lock();
+    if (isThreadRunning())
+    {
+        stopThread();
+    }
+
+    lib_cleanup();
+    unlock();
+}
+
+void ofxMQTT::reinitialise(string clientID, bool cleanSession)
+{
+    lock();
+    this->clientID = clientID;
+    check_error(mosquittopp::reinitialise(clientID.c_str(), cleanSession));
+    unlock();
+}
+
+void ofxMQTT::setup(string host, int port, int keepAlive)
+{
+    lock();
+    this->host = host;
+    this->port = port;
+    this->keepAlive = keepAlive;
+    unlock();
+}
+
+void ofxMQTT::connect()
+{
+    check_error(mosquittopp::connect(host.c_str(), port, keepAlive));
+    start();
+}
+
+void ofxMQTT::connect(string bindAddress)
+{
+    check_error(mosquittopp::connect(host.c_str(), port, keepAlive, bindAddress.c_str()));
+    start();
+}
+
+void ofxMQTT::reconnect()
+{
+    check_error(mosquittopp::reconnect());
+}
+
+void ofxMQTT::disconnect()
+{
+    stop();
+    check_error(mosquittopp::disconnect());
+}
+
+void ofxMQTT::publish(int mid, string topic, string payload, int qos, bool retain)
+{
+    check_error(mosquittopp::publish(&mid, topic.c_str(), payload.size(), payload.c_str(), qos, retain));
+}
+
+void ofxMQTT::subscribe(int mid, string sub, int qos)
+{
+    check_error(mosquittopp::subscribe(&mid, sub.c_str(), qos));
+}
+
+void ofxMQTT::unsubscribe(int mid, string sub)
+{
+    check_error(mosquittopp::unsubscribe(&mid, sub.c_str()));
+}
+
+void ofxMQTT::start()
+{
+    lock();
+    startThread(true, false);
+    unlock();
+}
+
+void ofxMQTT::stop()
+{
+    lock();
+    stopThread();
+    unlock();
+}
+
+void ofxMQTT::setUsernameAndPassword(string username, string password)
+{
+    lock();
+    this->username = username;
+    this->password = password;
+    check_error(username_pw_set(username.c_str(), password.c_str()));
+    unlock();
+}
+
+void ofxMQTT::setKeepAlive(int keepAlive)
+{
+    lock();
+    this->keepAlive = keepAlive;
+    unlock();
+}
+
+void ofxMQTT::setAutoReconnect(bool reconnect)
+{
+    lock();
+    this->bAutoReconnect = reconnect;
+    unlock();
+}
+
+void ofxMQTT::setUserdata(void *userdata)
+{
+    lock();
+    this->userdata = userdata;
+    unlock();
+}
+
+void ofxMQTT::setTls(string cafile, string capath, string certfile, string keyfile, string keyfilePath)
+{
+    int ret;
+    if (!keyfile.empty() && !keyfilePath.empty())
+    {
+        ofxMQTT::keyfilePath = keyfilePath;
+        check_error(tls_set(cafile.c_str(), capath.c_str(), certfile.c_str(), keyfile.c_str(), *pw_callback));
+    }
+    else
+    {
+        check_error(tls_set(cafile.c_str(), capath.c_str(), certfile.c_str()));
+    }
+}
+
+void ofxMQTT::setTlsOptions(int verifyMode, string version, string ciphers)
+{
+    check_error(tls_opts_set(verifyMode, version.c_str(), ciphers.c_str()));
+}
+
+void ofxMQTT::setTlsInsecure(bool insecure)
+{
+    check_error(tls_insecure_set(insecure));
+}
+
+void ofxMQTT::setPSK(string psk, string identity, string ciphers)
+{
+    check_error(tls_psk_set(psk.c_str(), identity.c_str(), ciphers.c_str()));
+}
+
+void ofxMQTT::threadedFunction()
+{
+    while (isThreadRunning())
+    {
+        if (lock())
+        {
+            int rc = loop();
+            if (MOSQ_ERR_SUCCESS != rc && bAutoReconnect)
+            {
+                ofLogError("ofxMQTT") << mosqpp::strerror(rc);
+                reconnect();
+                ofSleepMillis(20);
+            }
+            unlock();
+        }
+    }
+}
+
+void ofxMQTT::on_connect(int rc)
+{
+    if (MOSQ_ERR_SUCCESS == rc)
+    {
+        bConnected = true;
+    } else ofLogError("ofxMQTT") << mosqpp::strerror(rc);
+
+    ofNotifyEvent(onConnect, rc, this);
+}
+
+void ofxMQTT::on_disconnect(int rc)
+{
+    if (MOSQ_ERR_SUCCESS == rc)
+    {
+        bConnected = false;
+    } else ofLogError("ofxMQTT") << mosqpp::strerror(rc);
+
+    ofNotifyEvent(onDisconnect, rc, this);
+}
+
+void ofxMQTT::on_message(const struct mosquitto_message *message)
+{
+    ofxMQTTMessage msg;
+    msg.mid = message->mid;
+    msg.topic = message->topic;
+    msg.payload = message->payload;
+    msg.payloadlen = message->payloadlen;
+    msg.qos = message->qos;
+    msg.retain = message->retain;
+
+    ofNotifyEvent(onMessage, msg, this);
+}
+
+void ofxMQTT::on_publish(int rc)
+{
+    ofNotifyEvent(onPublish, rc, this);
+}
+
+void ofxMQTT::on_subscribe(int mid, int qos_count, const int *granted_qos)
+{
+    ofNotifyEvent(onSubscribe, mid, this);
+}
+
+void ofxMQTT::on_unsubscribe(int mid)
+{
+    ofNotifyEvent(onUnsubscribe, mid, this);
+}
+
+void ofxMQTT::on_log(int level, const char *str)
+{
+    ofLogVerbose("ofxMQTT") << "on_log : level = " << level << ", str = " << ofToString(str);
+}
+
+void ofxMQTT::on_error()
+{
+    ofLogError("ofxMQTT") << "error";
+}
+
+void ofxMQTT::check_error(int ret)
+{
+    if (0 < ret) ofLogError("ofxMQTT") << mosqpp::strerror(ret);
+}
---
+++ update.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+echo "clean workspace"
+rm -rf ./mosquitto
+rm -rf ./libs/mosquitto
+mkdir -p ./libs/mosquitto/src
+
+echo "clone mosquitto library"
+git clone http://git.eclipse.org/gitroot/mosquitto/org.eclipse.mosquitto.git ./mosquitto
+
+echo "copy mosquitto files"
+cp ./mosquitto/config.h ./libs/mosquitto/src/config.h
+cp ./mosquitto/lib/*.h ./libs/mosquitto/src/
+cp ./mosquitto/lib/*.c ./libs/mosquitto/src/
+cp ./mosquitto/lib/cpp/*.h ./libs/mosquitto/src/
+cp ./mosquitto/lib/cpp/*.cpp ./libs/mosquitto/src/
+
+echo "remove temporary files"
+rm -rf ./mosquitto
